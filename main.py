48ca9b9b96359582ec46410ca0e6ad41e661b78d
FarahGamal <farah.ali02@eng-st.cu.edu.eg>
2022-10-09 00:36:37 +0200

    Done

--- main.py
+++ main.py
@@ -1,48 +1,47 @@
 ########## Imports ##########
-import pathlib
-import sys, os
-from PIL import Image
-import pydicom as dicom
-from PyQt5 import QtCore, QtGui, QtWidgets,uic
-from PyQt5.QtWidgets import QApplication,QMainWindow, QWidget, QVBoxLayout, QPushButton, QFileDialog , QLabel, QTextEdit
-from PyQt5.QtGui import QIcon, QPixmap
-from GUI import Ui_MainWindow
-import matplotlib.pyplot as plt
 
-from matplotlib.figure import Figure
+from PIL import Image
+import sys, os, pathlib
+import pydicom as dicom
+import matplotlib.pyplot as plt
+from PyQt5 import  QtWidgets,uic
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
-#! -------------------------------------------------------------------------------------------------------------------------------------------------- #
 
 
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
         uic.loadUi('GUI.ui', self)
-
         self.show()
 
+        #?######### Initializations ##########
+
+        #* Bit depth dictionary 
         self.imageColorDictionary = {'1':1, 'L':8, 'P':8, 'RGB':24, 'RGBA':32, 'CMYK':32, 'YCbCr':24, 'I':32, 'F':32}
-        self.imageGraphicsView.hide()
+
+        #* Canvas definition 
         self.figure = plt.figure(figsize=(15,5))
         self.Canvas = FigureCanvas(self.figure)
-        self.gridLayout_2.addWidget(self.Canvas,0, 0, 1, 1)
-        #?######### Links of GUI Elements to Methods ##########
+        self.gridLayout.addWidget(self.Canvas,0, 0, 1, 1)
+
+        #!?######### Links of GUI Elements to Methods ##########
 
         self.pushButton.clicked.connect(self.openImage)
 
 
 
-#! ------------------------------------------------------------------------------------------------------------------------------------------------ #
-
-
 #?### Main Methods ####
 
-    #? Browse and get image path then call a specific function according to image format
+    #! Browse and get image path then call a specific function according to image format
     def openImage(self):
         print('0')
-        fileName = QtWidgets.QFileDialog.getOpenFileName(self, 'Open image','c:/', "Image files (*.jpg *.jpeg *.bmp *.dcm)")
+
+        #* Get image path
+        fileName = QtWidgets.QFileDialog.getOpenFileName(self, 'Open image','D:\FALL22\SBEN324\Task#1\Image-Viewer\images', "Image files (*.jpg *.jpeg *.bmp *.dcm)")
         imagePath = fileName[0]
+
+        #* Check image format then call its function
         if pathlib.Path(imagePath).suffix == ".jpg":
             self.jpgFormat(imagePath)
         elif pathlib.Path(imagePath).suffix == ".bmp":
@@ -50,74 +49,68 @@ class Ui(QtWidgets.QMainWindow):
         elif pathlib.Path(imagePath).suffix == ".dcm":
             self.dicomFormat(imagePath)
     
-    #? View jpg image format and show its attributes
+    #! View jpg image format and show its attributes
     def jpgFormat(self, imagePath):
         print('jpg')
-        # self.figure = plt.figure(figsize=(15,5))
-        # self.Canvas = FigureCanvas(self.figure)
-        # self.gridLayout_2.addWidget(self.Canvas,0, 0, 1, 1)
 
+        #* Open image then view it 
         image = Image.open(imagePath)
         plt.imshow(image)
-
         self.Canvas.draw()
-        
-        # self.viewImage(imagePath)
+
+        #* Hide DICM attribute
         self.hideDicomAttribute()
+
+        #* Show attributes of the image
         self.nuberOfRowsResultLable.setText(f'{image.width}')
         self.numberOfColumnsResultLable.setText(f'{image.height}')
         self.imageColorResultLable.setText(f'{image.mode}')
-        self.totalSizeResultLable.setText(f'{os.path.getsize(imagePath)}')
+        self.totalSizeResultLable.setText(f'{8*(os.path.getsize(imagePath))}')
         self.bitDepthResultLable.setText(f'{self.imageColorDictionary[image.mode]}')
 
-    #? View bmp image format and show its attributes
+    #! View bmp image format and show its attributes
     def bmpFormat(self, imagePath):
         print('bmp')
-        self.Canvas.hide()
-        self.imageGraphicsView.show()
+
+        #* Open image then view it 
         image = Image.open(imagePath)
-        self.viewImage(imagePath)
+        plt.imshow(image)
+        self.Canvas.draw()
+
+        #* Hide DICM attribute
         self.hideDicomAttribute()
+
+        #* Show attributes of the image
         self.nuberOfRowsResultLable.setText(f'{image.width}')
         self.numberOfColumnsResultLable.setText(f'{image.height}')
-        self.imageColorResultLable.setText(f'{image.mode}') ###
-        self.totalSizeResultLable.setText(f'{os.path.getsize(imagePath)}')
+        self.imageColorResultLable.setText(f'{image.mode}') 
+        self.totalSizeResultLable.setText(f'{8*(os.path.getsize(imagePath))}')
         self.bitDepthResultLable.setText(f'{self.imageColorDictionary[image.mode]}') ###
 
-    #? View DICOM image format and show its attributes 
+    #! View DICOM image format and show its attributes 
     def dicomFormat(self, imagePath):
         print('dicom')
 
-        self.imageGraphicsView.hide()
-        self.figure = plt.figure(figsize=(15,5))
-        self.Canvas = FigureCanvas(self.figure)
-        self.gridLayout_2.addWidget(self.Canvas,0, 0, 1, 1)
-
+        #* Open image then view it 
         image = dicom.dcmread(imagePath)
         plt.imshow(image.pixel_array,cmap=plt.cm.gray)
-
         self.Canvas.draw()
 
+        #* Show DICM attribute
         self.showDicomAttribute()
 
+        #* Show attributes of the image
         self.patientNameResultLable.setText(f'{image.PatientName}')
         self.modalityResultLable.setText(f'{image.Modality}')
         self.bodyPartExaminedResultLable.setText(f'{image.StudyDescription}')
         self.patientAgeResultLable.setText(f'{image.PatientAge}')
         self.nuberOfRowsResultLable.setText(f'{image.Rows}')
         self.numberOfColumnsResultLable.setText(f'{image.Columns}')
-        self.totalSizeResultLable.setText(f'{os.path.getsize(imagePath)}')
+        self.totalSizeResultLable.setText(f'{os.path.getsize(imagePath)}')  ###
         self.bitDepthResultLable.setText(f'{image.BitsAllocated}')
         self.imageColorResultLable.setText(f'{image.PhotometricInterpretation}')
 
-    #? Helper function to view jpg and bmp image formate
-    def viewImage(self, imagePath):
-        pix = QPixmap(imagePath)
-        item = QtWidgets.QGraphicsPixmapItem(pix)
-        scene = QtWidgets.QGraphicsScene(self)
-        scene.addItem(item)
-        self.imageGraphicsView.setScene(scene)
-
+    #! Hide all the attributes of the DICM image
     def hideDicomAttribute(self):
         self.patientNameResultLable.hide()
         self.modalityResultLable.hide()
@@ -128,6 +121,7 @@ class Ui(QtWidgets.QMainWindow):
         self.bodyPartExaminedTitleLable.hide()
         self.patientAgeTitleLable.hide()
 
+    #! Show all the attributes of the DICM image
     def showDicomAttribute(self):
         self.patientNameResultLable.show()
         self.modalityResultLable.show()
