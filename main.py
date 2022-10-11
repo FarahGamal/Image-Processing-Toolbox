4d49d1192c772e1c32ab7bc0d68f3a59e7342474
FarahGamal <farah.ali02@eng-st.cu.edu.eg>
2022-10-12 01:59:04 +0200

    final

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 ########## Imports ##########
 
+from tkinter import Canvas
 from PIL import Image
-import sys, os, pathlib
+import sys, os, pathlib, magic
 import pydicom as dicom
 import matplotlib.pyplot as plt
 from PyQt5 import  QtWidgets,uic
@@ -38,20 +39,16 @@ class Ui(QtWidgets.QMainWindow):
         print('0')
 
         #* Get image path
-        fileName = QtWidgets.QFileDialog.getOpenFileName(self, 'Open image','D:\FALL22\SBEN324\Task#1\Image-Viewer\images', "Image files (*.jpg *.jpeg *.bmp *.dcm)")
+        fileName = QtWidgets.QFileDialog.getOpenFileName(self, 'Open image','D:\FALL22\SBEN324\Task#1\Image-Viewer\images', "Image files (*.jpg *.jpeg *.bmp *.dcm *.png)")
         imagePath = fileName[0]
-
         #* Check image format then call its function
-        if pathlib.Path(imagePath).suffix == ".jpg":
-            self.jpgFormat(imagePath)
-        elif pathlib.Path(imagePath).suffix == ".bmp":
-            self.bmpFormat(imagePath)
-        elif pathlib.Path(imagePath).suffix == ".dcm":
+        magic.from_file(imagePath)
+        if magic.from_file(imagePath) == 'DICOM medical imaging data':
             self.dicomFormat(imagePath)
+        else: self.jpgAndBmpFormat(imagePath)
     
-    #! View jpg image format and show its attributes
-    def jpgFormat(self, imagePath):
-        print('jpg')
+    #! View jpg and bmp image format and show its attributes
+    def jpgAndBmpFormat(self, imagePath):
 
         #* Open image then view it 
         image = Image.open(imagePath)
@@ -68,25 +65,6 @@ class Ui(QtWidgets.QMainWindow):
         self.totalSizeResultLable.setText(f'{8*(os.path.getsize(imagePath))}')
         self.bitDepthResultLable.setText(f'{self.imageColorDictionary[image.mode]}')
 
-    #! View bmp image format and show its attributes
-    def bmpFormat(self, imagePath):
-        print('bmp')
-
-        #* Open image then view it 
-        image = Image.open(imagePath)
-        plt.imshow(image)
-        self.Canvas.draw()
-
-        #* Hide DICM attribute
-        self.hideDicomAttribute()
-
-        #* Show attributes of the image
-        self.nuberOfRowsResultLable.setText(f'{image.width}')
-        self.numberOfColumnsResultLable.setText(f'{image.height}')
-        self.imageColorResultLable.setText(f'{image.mode}') 
-        self.totalSizeResultLable.setText(f'{8*(os.path.getsize(imagePath))}')
-        self.bitDepthResultLable.setText(f'{self.imageColorDictionary[image.mode]}') ###
-
     #! View DICOM image format and show its attributes 
     def dicomFormat(self, imagePath):
         print('dicom')
@@ -100,15 +78,23 @@ class Ui(QtWidgets.QMainWindow):
         self.showDicomAttribute()
 
         #* Show attributes of the image
-        self.patientNameResultLable.setText(f'{image.PatientName}')
-        self.modalityResultLable.setText(f'{image.Modality}')
-        self.bodyPartExaminedResultLable.setText(f'{image.StudyDescription}')
-        self.patientAgeResultLable.setText(f'{image.PatientAge}')
-        self.nuberOfRowsResultLable.setText(f'{image.Rows}')
-        self.numberOfColumnsResultLable.setText(f'{image.Columns}')
-        self.totalSizeResultLable.setText(f'{os.path.getsize(imagePath)}')  ###
-        self.bitDepthResultLable.setText(f'{image.BitsAllocated}')
-        self.imageColorResultLable.setText(f'{image.PhotometricInterpretation}')
+        if hasattr(image, 'Modality'): self.modalityResultLable.setText(f'{image.Modality}')
+        else : self.modalityResultLable.setText('------')
+        if hasattr(image, 'StudyDescription'): self.bodyPartExaminedResultLable.setText(f'{image.StudyDescription}')
+        else : self.bodyPartExaminedResultLable.setText('------')
+        if hasattr(image, 'PatientAge'): self.patientAgeResultLable.setText(f'{image.PatientAge}')
+        else : self.patientAgeResultLable.setText('------')
+        if hasattr(image, 'Rows'): self.nuberOfRowsResultLable.setText(f'{image.Rows}')
+        else : self.nuberOfRowsResultLable.setText('------')
+        if hasattr(image, 'Columns'): self.numberOfColumnsResultLable.setText(f'{image.Columns}')
+        else : self.numberOfColumnsResultLable.setText('------')
+        if hasattr(image, 'BitsAllocated'): self.bitDepthResultLable.setText(f'{image.BitsAllocated}')
+        else : self.bitDepthResultLable.setText('------')
+        if hasattr(image, 'PhotometricInterpretation'): self.imageColorResultLable.setText(f'{image.PhotometricInterpretation}')
+        else : self.imageColorResultLable.setText('------')
+        if hasattr(image, 'PatientName'): self.patientNameResultLable.setText(f'{image.PatientName}')
+        else : self.patientNameResultLable.setText('------')
+        self.totalSizeResultLable.setText(f'{os.path.getsize(imagePath)}')  
 
     #! Hide all the attributes of the DICM image
     def hideDicomAttribute(self):
