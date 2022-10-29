87ac1d0fcdf75b497c1106dbeaeb7266c107254b
FarahGamal <farah.ali02@eng-st.cu.edu.eg>
2022-10-29 19:39:16 +0200

    task3

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from PyQt5.QtWidgets import *
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from PyQt5 import  QtWidgets,uic
+import cv2 as cv
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 #?-----------------------------------------------------------------------------------------------------------------------------# 
@@ -25,6 +26,7 @@ class Ui(QtWidgets.QMainWindow):
         self.browseButton.clicked.connect(self.openImage)
         self.browsePushButton.clicked.connect(self.openImageZoomTab)
         self.zoomPushButton.clicked.connect(self.zoom)
+        self.generateTLetterPushButton.clicked.connect(self.generateTLetter)
 
 #?-----------------------------------------------------------------------------------------------------------------------------#
 
@@ -371,7 +373,111 @@ class Ui(QtWidgets.QMainWindow):
         else:
             val = round(val)
         return val
+
+#?-----------------------------------------------------------------------------------------------------------------------------#
+
+                                                #?######## Task 2 Functions  #########
     
+    def generateTLetter(self):
+        width = 128
+        height = 128
+
+        image = (width, height, 3)
+        array = np.zeros(image, dtype=np.uint8)
+
+        for i in range(28, 48):
+            for j in range(28, 98):
+                array[i,j] = [255, 255, 255]
+
+        for i in range(48, 98):
+            for j in range(53, 73):
+                array[i,j] = [255, 255, 255]
+
+        img = Image.fromarray(array)
+
+        self.figure = plt.figure(figsize=(15,5))
+        self.Canvas = FigureCanvas(self.figure)
+        self.tLetterGridLayout.addWidget(self.Canvas,0, 0, 1, 1)
+
+        plt.imshow(img, cmap='gray')
+        self.Canvas.draw()
+        rotatedImage, new_height, new_width, output = self.rotate(array, 80)
+        # test = self.bilinearInterpolation(array, 128,128,output,new_width,new_height) 
+        # interpolate = Image.fromarray(np.uint8(test))
+        self.figure = plt.figure(figsize=(15,5))
+        self.Canvas = FigureCanvas(self.figure)
+        self.rotatedImageGridLayout.addWidget(self.Canvas,0, 0, 1, 1)
+
+        plt.imshow(rotatedImage, cmap='gray')
+        self.Canvas.draw()
+    
+    def shear(self, angle,x,y):
+        # shear 1
+        tangent=math.tan(angle/2)
+        new_x=round(x-y*tangent)
+        new_y=y
+        #shear 2
+        new_y=round(new_x*math.sin(angle)+new_y)      #since there is no change in new_x according to the shear matrix
+        #shear 3
+        new_x=round(new_x-new_y*tangent)              #since there is no change in new_y according to the shear matrix
+        return new_y,new_x
+
+    def rotate(self, image,a): 
+    #image : your image
+    #a : angle to rotate your image
+        angle = - ( a )            
+        angle=math.radians(angle)                               
+        cosine=math.cos(angle)
+        sine=math.sin(angle)
+        height=image.shape[0]    
+        width=image.shape[1]                                  
+        # height and width of the new image 
+        new_height  = round(abs(image.shape[0]*cosine)+abs(image.shape[1]*sine))+1
+        new_width  = round(abs(image.shape[1]*cosine)+abs(image.shape[0]*sine))+1
+        #image variable of dimensions of new_height and new _column filled with zeros
+        output=np.zeros((new_height,new_width,image.shape[2]))
+        image_copy=output.copy()
+        #Find the centre of the image about which we have to rotate the image
+        original_centre_height   = round(((image.shape[0]+1)/2)-1)    
+        original_centre_width    = round(((image.shape[1]+1)/2)-1)   
+        # Find the centre of the new image that will be obtained
+        new_centre_height= round(((new_height+1)/2)-1)        
+        new_centre_width= round(((new_width+1)/2)-1)          
+
+        for i in range(height):
+            for j in range(width):
+                #co-ordinates of pixel with respect to the centre of original image
+                y=image.shape[0]-1-i-original_centre_height                   
+                x=image.shape[1]-1-j-original_centre_width 
+                '''
+                #co-ordinate of pixel with respect to the rotated image
+            
+                new_y=round(-x*sine+y*cosine)
+                new_x=round(x*cosine+y*sine)
+                '''
+                
+                new_y,new_x = self.shear(angle,x,y)
+                new_y=new_centre_height-new_y
+                new_x=new_centre_width-new_x
+            
+                if 0 <= new_x < new_width and 0 <= new_y < new_height and new_x>=0 and new_y>=0:
+
+                    output[new_y,new_x,:]=image[i,j,:] 
+        pil_img=Image.fromarray((output).astype(np.uint8))      
+
+        pil_img.save("rotated_image.png")    
+        img=cv.imread("rotated_image.png")
+        img_2=cv.imread("img.jpg")
+
+        # plt.imshow(img, cmap='gray')
+        # plt.imshow(img, cmap='gray')
+        # cv.imshow("Befor Rotate",img_2)
+        # cv.imshow("After Rotate",img)
+        cv.waitKey(0)
+        return img, new_height, new_width, output
+          
+    
+
 #?-----------------------------------------------------------------------------------------------------------------------------#
 
                                                 #?######## Helper Functions #########
